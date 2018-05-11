9966a1ff1f71be90567f191cf96df63b78eab2ab
Ubirajara Cortes <birapjr@gmail.com>
2018-05-11 22:21:11 +0200

    Express app started.

--- blocks_horizontal/arduino.js
+++ blocks_horizontal/arduino.js
@@ -2,7 +2,7 @@
  * @license
  * Visual Blocks Editor
  *
- * Copyright 2016 Massachusetts Institute of Technology
+ * Copyright 2018 TrendsMix Technology
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
