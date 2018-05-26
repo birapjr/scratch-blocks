83c761c4b0f9a359b85cc34296cd63566bea6854
Ubirajara Cortes <birapjr@gmail.com>
2018-05-26 22:49:44 +0200

    Transpiler code under progress.

--- transpiler/transpiler.js
+++ transpiler/transpiler.js
@@ -47,56 +47,140 @@ delay(1000);                       // wait for a second
 }`
 
 /**
- * Get a C code of a block
- * @param {Object} block The current block to parse
+ * Get the node of the next block to process
+ * @param {Object} jsonCodeObject The blocks code
+ * @param {String} lastBlockId The last processed block id
  */
-function getBlockCode(block) {
-  let code;
-  let lastCode;
-  switch (block.$.type) {
-    case 'control_forever':
-      lastCode = block.$.type;
-      code = 'while(1){';
-      break;
-    case 'arduino_setcolor':
-      //get the status of the block
-      let status = block.value[0].shadow[0].field[0]._;
-      break;
-  }
-
-  if (lastCode == 'control_forever') {
-    if (block.statement && block.statement.length) {
-      code += getBlockCode(block.statement[0].block[0]);
+function getNextSourceBlock(jsonCodeObject, lastBlockId) {
+  let nextBlock;
+  if (lastBlockId != null) {
+    //search the next block
+    if (jsonCodeObject.xml) {
+      if (jsonCodeObject.xml.block[0].$.id == lastBlockId) {
+        //call recursive
+        nextBlock = getNextSourceBlock(jsonCodeObject.xml.block[0], lastBlockId);
+      }
+      else {
+        //call recursive again
+        if (jsonCodeObject.xml) {
+          nextBlock = getNextSourceBlock(jsonCodeObject.xml.block[0], lastBlockId);
+        }
+      }
+    }
+    else if (jsonCodeObject.$.id == lastBlockId) {
+      if (jsonCodeObject.next && jsonCodeObject.next[0] && jsonCodeObject.next[0].block && jsonCodeObject.next[0].block[0]) {
+        nextBlock = jsonCodeObject.next[0].block[0];
+      }
+      else if (jsonCodeObject.statement && jsonCodeObject.statement[0]) {
+        nextBlock = jsonCodeObject.statement[0].block[0];
+      }
+    }
+    else if ((jsonCodeObject.next)) {
+      //get next
+      nextBlock = getNextSourceBlock(jsonCodeObject.next[0].block[0], lastBlockId);
     }
   }
+  else {
+    //Get the first block
+    if (jsonCodeObject.xml.block[0].$) {
+      nextBlock = jsonCodeObject.xml.block[0];
+    }
+  }
+  return nextBlock;
+}
 
-  return code;
+/**
+ * Get properties of a block and its code
+ * @param {Object} jsonCodeObject The blocks code
+ * @param {String} lastBlockId The last processed block id
+ */
+function getNextBlock(jsonCodeObject, lastBlockId) {
+  let nextBlock = {
+    code: null,
+    id: null,
+    type: null,
+    value: null
+  }
+
+  let nextSourceBlock = getNextSourceBlock(jsonCodeObject, lastBlockId);
+  switch (nextSourceBlock.$.type) {
+    //green start flag
+    case 'event_whenflagclicked':
+      nextBlock.type = 'event_whenflagclicked';
+      nextBlock.id = nextSourceBlock.$.id;
+      break;
+
+    case 'control_forever':
+      nextBlock.type = 'control_forever';
+      nextBlock.id = nextSourceBlock.$.id;
+      nextBlock.code = 'while (1) {';
+      break;
+
+    case 'arduino_setcolor':
+      nextBlock.type = 'arduino_setcolor';
+      nextBlock.id = nextSourceBlock.$.id;
+      nextBlock.value = nextSourceBlock.value[0].shadow[0].field[0]._;
+      break;
+  
+    default:
+      break;
+  }
+
+  return nextBlock;
 }
 
 /**
  * Transpile to Google Block code into Arduino C
- * @param {Object} jsonCodeObejct 
+ * @param {Object} jsonCodeObject 
  * 
  * @returns {String} the transpiled C code
  */
-function transpile(jsonCodeObejct) {
-  let cCode;
+function transpile(jsonCodeObject) {
+  //c transpiled code parts
+  let cGlobalCode;
+  let cSetupCode;
+  let cLoopCode;
 
-  if (jsonCodeObejct.xml) {
-    if (jsonCodeObejct.xml.block && jsonCodeObejct.xml.block.length) {
-      //green start flag
-      if (jsonCodeObejct.xml.block[0].$.type == 'event_whenflagclicked') {
-        let next = jsonCodeObejct.xml.block[0].next;
-        if (next && next.length) {
-          cCode += getBlockCode(next[0].block[0]);
-        }
-      }
+  //current block
+  let lastBlockId;
+  let currentBlock;
+
+  //end/start of code found
+  let endOfCode = false;
+  let startOfCode = false;
+
+  let codeBlockMap = new Map();
+  let sequence = 1;
+
+  while (!endOfCode) {
+    currentBlock = getNextBlock(jsonCodeObject, lastBlockId);
+
+    codeBlockMap.set(sequence, currentBlock);
+    sequence++;
+
+    if (currentBlock && currentBlock.id) {
+      lastBlockId = currentBlock.id;
+    }
+
+    switch (currentBlock.type) {
+      case 'control_forever':
+        cLoopCode += currentBlock.code;
+        break;
+      case 'arduino_setcolor':
+        //get the status of the block
+        break;
+      case 'event_whenflagclicked':
+        startOfCode = true;
+        break;
+    }
+
+    if (!startOfCode) {
+      endOfCode = true;
+      console.log("Start block not found");
     }
   }
 
-  cCode = basicScractArduino;
-
-  return cCode;
+  return `${cGlobalCode}${cSetupCode}${cLoopCode}`;
 }
 
 module.exports = {
